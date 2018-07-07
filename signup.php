2b923f5f97b430fad1c29c9ce939b0b677a0b631
tehrabbitt@gmail.com <tehrabbitt@gmail.com>
2018-07-07 02:43:17 +0000

    Signup Page being added.

---
+++ signup.php
@@ -0,0 +1,20 @@
+
+<h1 style="text-align: center;"><img src="toool_logo.svg" alt="" width="472" height="184" /></h2>
+<h1 style="text-align: center;"><strong>Contestant Signup Form</strong></h1>
+<table style="height: 173px; width: 199px; margin-left: auto; margin-right: auto;" border="2">
+<tbody>
+<tr style="height: 100.266px;">
+<td style="width: 187px; height: 100.266px;">
+<form action="/register.php" method="post">
+<p>Handle:<br /> <input name="handle" type="text" /><br /><br /> 
+  Difficulty Level:<br /> 
+  <input checked="checked" name="difficulty" type="radio" value="1" /> Easy<br /> 
+  <input name="difficulty" type="radio" value="2" /> Medium<br /> 
+  <input name="difficulty" type="radio" value="3" /> Hard</p>
+  <input name="difficulty" type="radio" value="4" /> <font color="red">Expert</font></p>
+<p><input type="submit" value="Print Barcode" /></p>
+</form></td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
