0aedc7cf7e5c5d95a589e6e8a47b4b1ab8c5f1fd
tehrabbitt@gmail.com <tehrabbitt@gmail.com>
2018-07-07 06:51:24 +0000

    added new start screen

---
+++ start.php
@@ -0,0 +1,17 @@
+
+<h1 style="text-align: center;"><img src="toool_logo.svg" alt="" width="472" height="184" /></h2>
+<h1 style="text-align: center;"><strong>Lockpicking Challenge</strong></h1>
+<table style="height: 173px; width: 199px; margin-left: auto; margin-right: auto;" border="2">
+<tbody>
+<tr style="height: 100.266px;">
+<td style="width: 187px; height: 100.266px;">
+<form action="timer2.php" method="get">
+<br />
+<p align = "center"><b>Enter Barcode:</b><br /> <input name="code" type="text" /><br /><br />
+  <p align = "left">
+<p align = "center"><input type="submit" value="Start the Clock!"/></p>
+</form></td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
